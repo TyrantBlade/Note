218c0f3a1c47e09a60673b246874926748fb11cb
TyrantBlade <germanius02@gmail.com>
2019-06-10 07:39:42 -0400

    Ver 2.0 prototype

--- app/src/main/java/com/example/note/MainActivity.java
+++ app/src/main/java/com/example/note/MainActivity.java
@@ -10,27 +10,31 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private File root,gpxfile;
+    private String filPath;
     FileWriter writer;
+    FileReader reader;
     private EditText txView;
-    public int i;
 
     private static final int REQUEST_CODE_PATH=1;
 
     //
     private ImageView imNew,imOpen,imDel,imSave;
+    private BufferedReader bufferedReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        i=0;
 
         txView=findViewById(R.id.textAff);
 
@@ -39,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         imOpen.setOnClickListener(this);     imNew.setOnClickListener(this);
         imDel.setOnClickListener(this);     imSave.setOnClickListener(this);
+        if(gpxfile==null){
+            createFile(this,"default");
+        }
     }
 
     @Override
@@ -56,6 +63,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             intent.putExtra(ListFolder.OPERATION,false);
             startActivityForResult(intent,REQUEST_CODE_PATH);
         }
+        if(v==imSave){
+           String data = txView.getText().toString();
+            saveFile(data);
+        }
+    }
+
+    private void saveFile(String data) {
+        try {
+            writer = new FileWriter(gpxfile);
+            writer.append(data);
+            writer.flush();
+            writer.close();
+            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -63,7 +87,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (requestCode==REQUEST_CODE_PATH){
             if (resultCode==RESULT_OK){
                 String path=data.getStringExtra(ListFolder.RESULT_PATH_FILE);
-                Toast.makeText(this, path, Toast.LENGTH_LONG).show();
+                this.filPath=path;
+                txView.setText(readFile(filPath));
             }
             else{
                 Toast.makeText(this, "No file to open", Toast.LENGTH_SHORT).show();
@@ -74,26 +99,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void createFile(Context context, String sBody) {
+    public void createFile(Context context, String name){
         try {
             root = new File(Environment.getExternalStorageDirectory(), "Notes");
             if (!root.exists()) {
                 root.mkdirs();
             }
-            String nomFichier="Note ";
-            String num=String.valueOf(i);
-
-            nomFichier=nomFichier+num;
-
-            gpxfile = new File(root, nomFichier+".txt");
+            gpxfile = new File(root, name+".txt");
             writer = new FileWriter(gpxfile);
-            writer.append(sBody);
+            writer.append("/-/------Default File------/-/");
+            txView.setText("/-/------Default File------/-/");
             writer.flush();
-            i++;
             writer.close();
-            Toast.makeText(context, "Saved", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, name+" just created", Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String readFile(String path){
+        gpxfile=new File(path);
+        StringBuilder text =new StringBuilder();
+        try {
+            reader=new FileReader(gpxfile);
+            bufferedReader = new BufferedReader(reader);
+
+            String line;
+            while ((line=bufferedReader.readLine())!=null){
+                text.append(line);
+                text.append("\n");
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text.toString();
     }
 }
